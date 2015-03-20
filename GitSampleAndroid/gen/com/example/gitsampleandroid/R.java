682ee190a49ecb06167dd8b96d1da55fde3362c2
siddik3436 <siddik3436@gmail.com>
2015-03-20 19:04:52 +0600

    add activity

--- GitSampleAndroid/gen/com/example/gitsampleandroid/R.java
+++ GitSampleAndroid/gen/com/example/gitsampleandroid/R.java
@@ -1740,7 +1740,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_second=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1801,6 +1802,7 @@ containing a value of this type.
         public static final int abc_toolbar_collapse_description=0x7f0a0004;
         public static final int app_name=0x7f0a0010;
         public static final int hello_world=0x7f0a0011;
+        public static final int title_activity_second=0x7f0a0012;
     }
     public static final class style {
         /** 
